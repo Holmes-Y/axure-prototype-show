2e88b001ced4a2f6f1568df4cf29f6daf74abc03
yangbowen <yholmes0@qq.com>
2023-04-12 09:08:10 +0800

    #

--- src/store/index.js
+++ src/store/index.js
@@ -15,7 +15,7 @@ const store = new Vuex.Store({
     },
     mutations: {
         addProjectInfo() {
-            state.projectInfo.set
+            state.projectInfo = {}
         }
     },
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,34 @@
         <el-container>
             <el-header>Header</el-header>
             <el-main>
+                <div class="btn-wrap">
+                    <el-button type="primary" @click="open">添加项目</el-button>
+                    <div style="margin: 20px"></div>
+                    <el-form label-width="80px" :model="formLabelAlign">
+                        <el-form-item label="项目名称">
+                            <el-input
+                                v-model="formLabelAlign.region"
+                            ></el-input>
+                        </el-form-item>
+                        <el-form-item label="应用类型">
+                            <el-select
+                                v-model="formLabelAlign.value"
+                                placeholder="请选择"
+                            >
+                                <el-option
+                                    v-for="item in formLabelAlign.options"
+                                    :key="item.value"
+                                    :label="item.label"
+                                    :value="item.value"
+                                >
+                                </el-option>
+                            </el-select>
+                        </el-form-item>
+                        <el-form-item label="项目说明">
+                            <el-input v-model="formLabelAlign.type"></el-input>
+                        </el-form-item>
+                    </el-form>
+                </div>
                 <ProjectsItem></ProjectsItem>
             </el-main>
         </el-container>
@@ -13,6 +41,64 @@
 import ProjectsItem from "../components/ProjectsItem.vue";
 export default {
     name: "HomeView",
+    data() {
+        return {
+            formLabelAlign: {
+                name: "",
+                region: "",
+                type: "",
+                options: [
+                    {
+                        value: "选项1",
+                        label: "小程序",
+                    },
+                    {
+                        value: "选项2",
+                        label: "APP",
+                    },
+                    {
+                        value: "选项3",
+                        label: "Web",
+                    },
+                ],
+                value: "",
+            },
+        };
+    },
+    methods: {
+        open() {
+            const h = this.$createElement;
+            this.$msgbox({
+                title: "添加项目",
+                message: h("p", null, [
+                    h("span", null, "内容可以是 "),
+                    h("i", { style: "color: teal" }, "VNode"),
+                ]),
+                showCancelButton: true,
+                confirmButtonText: "确定",
+                cancelButtonText: "取消",
+                beforeClose: (action, instance, done) => {
+                    if (action === "confirm") {
+                        instance.confirmButtonLoading = true;
+                        instance.confirmButtonText = "执行中...";
+                        setTimeout(() => {
+                            done();
+                            setTimeout(() => {
+                                instance.confirmButtonLoading = false;
+                            }, 300);
+                        }, 3000);
+                    } else {
+                        done();
+                    }
+                },
+            }).then((action) => {
+                this.$message({
+                    type: "info",
+                    message: "action: " + action,
+                });
+            });
+        },
+    },
     components: {
         ProjectsItem,
     },
@@ -37,10 +123,13 @@ export default {
 .el-main {
     background-color: #e9eef3;
     color: #333;
-    text-align: center;
-    line-height: 160px;
+    text-align: left;
+    // line-height: 160px;
 }
 
+.btn-wrap {
+    margin-bottom: 16px;
+}
 body > .el-container {
     margin-bottom: 40px;
 }
